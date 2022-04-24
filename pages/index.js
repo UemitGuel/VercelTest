3e55a38c3887ebdb7ce1a07090842c526338daf1
ümit.eth <47325422+UemitGuel@users.noreply.github.com>
2022-04-24 20:10:45 +0200

    Initial commit

---
+++ pages/index.js
@@ -0,0 +1,11 @@
+import { Box, Container, Heading, Text } from '@chakra-ui/react'
+
+export default function Home() {
+  return (
+    <Container>
+      <Box>
+        <Heading>This is my Template</Heading>
+      </Box>
+    </Container>
+  )
+}
